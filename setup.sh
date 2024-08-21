ca80b987f9a25be18e0c30aba1b1558321f4ef20
Bruno <112035641+BrunoCode69@users.noreply.github.com>
2024-08-21 18:07:06 -0300

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+port=$1;
+
+mkdir /bin/manage_users
+wget -P /bin/manage_users/ https://github.com/BrunoCode69/Whatsapp---Instances/raw/main/index
+
+echo "[Unit]
+Description=Manage Users Service
+After=network.target
+
+[Service]
+ExecStart=/bin/manage_users/index $port
+WorkingDirectory=/bin/manage_users/
+Restart=always
+Environment=NODE_ENV=production
+StandardOutput=append:/var/log/manage_users.log
+StandardError=append:/var/log/manage_users.log
+
+
+[Install]
+WantedBy=multi-user.target" > /lib/systemd/system/manage_users.service
+
+systemctl daemon-reload
+systemctl enable manage_users.service
+systemctl start manage_users.service
