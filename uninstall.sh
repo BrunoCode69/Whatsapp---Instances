b651dc04a057f062cb6d272bd4dabdaf66578adf
Bruno <112035641+BrunoCode69@users.noreply.github.com>
2024-08-21 18:43:13 -0700

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -2,6 +2,6 @@
 systemctl stop manange_users
 systemctl disable manange_users
 rm -f /etc/systemd/system/manange_users.service
-rm -f /bin/manage_users
+rm -r /bin/manage_users
 systemctl daemon-reload
 rm -r uninstall.sh
